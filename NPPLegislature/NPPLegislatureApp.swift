4a8609e997d42cac919cb095cea4f00d6c9eccfa
Rice Lin <rice.lin@icloud.com>
2022-12-30 00:24:16 +0800

    Add Xcode project.

---
+++ NPPLegislature/NPPLegislatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPPLegislatureApp.swift
+//  NPPLegislature
+//
+//  Created by Rice Lin on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct NPPLegislatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
